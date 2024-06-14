d38812feee7e041c70d97d9e5aa2b72cc861149a
Faruk Çelik <ahmet48.faruk@gmail.com>
2024-06-14 22:36:53 +0300

    #

---
+++ CoreWebProject.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebProject.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult PartialSidebar ()
+        {
+            return PartialView();
+        }
+        public IActionResult PartialFooter ()
+        {
+            return PartialView();
+        }
+        public IActionResult PartialNavbar ()
+        {
+            return PartialView();
+        }
+        public IActionResult PartialHead ()
+        {
+            return PartialView();
+        }
+        public IActionResult PartialScript ()
+        {
+            return PartialView();
+        }
+        public IActionResult PartialNavigation ()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ CoreWebProject.WebUI/Controllers/SkillController.cs
@@ -0,0 +1,36 @@
+﻿using CoreWebProject.BusinessLayer.Concrete;
+using CoreWebProject.DataAccessLayer.EntityFramework;
+using CoreWebProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebProject.WebUI.Controllers
+{
+    public class SkillController : Controller
+    {
+        SkillManager SkillManager = new SkillManager(new EfSkillDal());
+        public IActionResult Index ()
+        {
+            ViewBag.v1 = "List Skill";
+            ViewBag.v2 = "Skill";
+            ViewBag.v3 = "List Skill";
+
+            var values = SkillManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddSkill ()
+        {
+            ViewBag.v1 = "Add Skill";
+            ViewBag.v2 = "Skill";
+            ViewBag.v3 = "Add Skill";
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSkill (Skill skill)
+        {
+            SkillManager.TAdd(skill);
+            return RedirectToAction("Index");
+        }
+    }
+}
